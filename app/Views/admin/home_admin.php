a554adafe721d60b966836c72a652c6c8c70cd1b
BumbleBrice <collilieux.brice@gmail.com>
2016-06-22 12:25:14 +0200

    Mise en forme de la page admin OK

--- app/Views/admin/home_admin.php
+++ app/Views/admin/home_admin.php
@@ -1,21 +1,10 @@
 <?php $this->layout('layout_admin', ['title' => 'Accueil']) ?>
 
 <?php $this->start('main_content') ?>
-
-		<nav id="navigation" class="navbar navbar-fixed-top" role="navigation">
-                <ul class="sidebar-nav">
-                    <li class=""><a class="js-scrollTo" href="#presentation">Présentation</a></li>
-                    <li class=""><a class="js-scrollTo" href="#carte">Carte</a></li>
-                    <li class=""><a class="js-scrollTo" href="#bar_list">Liste des bars</a></li>
-                    <li class=""><a class="js-scrollTo" href="#bar_add">Ajouter un bar</a></li>
-                    <li class=""><a class="js-scrollTo" href="#message">Messages</a></li>
-                    <li class=""><a class="js-scrollTo" href="#users">Gestion utilisateurs</a></li>
-                </ul>
-            </nav>
-	<div class="module" id="presentation"><h1>Présentation</h1></div>
-	<div class="module" id="carte"><h1>Carte</h1></div>
-	<div class="module" id="bar_list"><h1>Liste des bars</h1></div>
-	<div class="module" id="bar_add"><h1>Ajouter un bar</h1></div>
-	<div class="module" id="message"><h1>Messagerie</h1></div>
-	<div class="module" id="users"><h1>Gestion des utilisateurs</h1></div>
+	<div class="module" id="presentation"><div class="contenue"><h1>Présentation</h1></div></div>
+	<div class="module" id="carte"><div class="contenue"><h1>Carte</h1></div></div>
+	<div class="module" id="bar_list"><div class="contenue"><h1>Liste des bars</h1></div></div>
+	<div class="module" id="bar_add"><div class="contenue"><h1>Ajouter un bar</h1></div></div>
+	<div class="module" id="message"><div class="contenue"><h1>Messagerie</h1></div></div>
+	<div class="module" id="users"><div class="contenue"><h1>Gestion des utilisateurs</h1></div></div>
 <?php $this->stop('main_content') ?>
--- app/Views/layout_admin.php
+++ app/Views/layout_admin.php
@@ -7,15 +7,26 @@
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/bootstrap.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/simple-sidebar.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style_admin.css') ?>">
-	<script rel="stylesheet" href="<?= $this->assetUrl('js/script.js') ?>"></script>
-	<script rel="stylesheet" href="<?= $this->assetUrl('js/jquery.easing.1.3.js') ?>"></script>
-
 </head>
 <body>
-	<div class="container ">
+	<div>
+	<aside role="banner" >
+		<nav id="navigation" class="navbar navbar-fixed-top" role="navigation">
+                <ul class="pam sidebar-nav">
+                    <li class=""><a class="js-scrollTo" href="#presentation">Présentation</a></li>
+                    <li class=""><a class="js-scrollTo" href="#carte">Carte</a></li>
+                    <li class=""><a class="js-scrollTo" href="#bar_list">Liste des bars</a></li>
+                    <li class=""><a class="js-scrollTo" href="#bar_add">Ajouter un bar</a></li>
+                    <li class=""><a class="js-scrollTo" href="#message">Messages</a></li>
+                    <li class=""><a class="js-scrollTo" href="#users">Gestion utilisateurs</a></li>
+                </ul>
+            </nav>
+	</aside>
 		<section class="wrapper"><?= $this->section('main_content') ?></section>
 	</div>
-	<script src="js/jquery.easing.1.3.js"></script>
-	<script src="js/script.js"></script>
+
+	<script src="<?= $this->assetUrl('js/jquery.js') ?>"></script>
+	<script src="<?= $this->assetUrl('js/script.js') ?>"></script>
+	<script src="<?= $this->assetUrl('js/jquery.easing.1.3.js') ?>"></script>
 </body>
 </html>
