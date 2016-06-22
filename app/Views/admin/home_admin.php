cbe40234f00718c045ce79d1baef9afaca9b724b
YoanGarcia <yoan.gcia@hotmail.fr>
2016-06-22 15:05:12 +0200

    ajout des emplacements à remplir ( par section )

--- app/Views/admin/home_admin.php
+++ app/Views/admin/home_admin.php
@@ -1,11 +1,68 @@
 <?php $this->layout('layout_admin', ['title' => 'Accueil']) ?>
 
 <?php $this->start('main_content') ?>
-	<div class="module" id="presentation"><div class="contenue"><h1>Présentation</h1></div></div>
-	<div class="module" id="carte"><div class="contenue"><h1>Carte</h1></div></div>
-	<div class="module" id="bar_list"><div class="contenue"><h1>Liste des bars</h1></div></div>
-	<div class="module" id="bar_add"><div class="contenue"><h1>Ajouter un bar</h1></div></div>
-	<div class="module" id="message"><div class="contenue"><h1>Messagerie</h1></div></div>
-	<div class="module" id="users"><div class="contenue"><h1>Gestion des utilisateurs</h1></div></div>
-	<div class="module" id="news_letter"><div class="contenue"><h1>Ajouter une news</h1></div></div>
+
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<!--                         PRESENTATION                        -->
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<div class="module" id="presentation">
+		<div class="contenue">
+			<h1>Présentation</h1>
+		</div>
+	</div>
+
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<!--                            CARTE                            -->
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<div class="module" id="carte">
+		<div class="contenue">
+			<h1>Carte</h1>
+		</div>
+	</div>
+
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<!--                        LISTE DES BARS                       -->
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<div class="module" id="bar_list">
+		<div class="contenue">
+			<h1>Liste des bars</h1>
+		</div>
+	</div>
+
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<!--                       AJOUTER DES BARS                      -->
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<div class="module" id="bar_add">
+		<div class="contenue">
+			<h1>Ajouter un bar</h1>
+		</div>
+	</div>
+
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<!--                       CONTACT / MESSAGE                     -->
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<div class="module" id="message">
+		<div class="contenue">
+			<h1>Messagerie</h1>
+		</div>
+	</div>
+
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<!--                         UTILISATEURS                        -->
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<div class="module" id="users">
+		<div class="contenue">
+			<h1>Gestion des utilisateurs</h1>
+		</div>
+	</div>
+
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<!--                         NEWS LETTER                         -->
+	<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<div class="module" id="news_letter">
+		<div class="contenue">
+			<h1>Ajouter une news</h1>
+		</div>
+	</div>
+
 <?php $this->stop('main_content') ?>
