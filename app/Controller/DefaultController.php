0bd2a1a6d8f9be22e094003c983d4306daa4955c
AlexisMeunier <meunier_33@live.fr>
2016-06-23 10:38:57 +0200

    aze

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -19,16 +19,16 @@ class DefaultController extends Controller
 
 			if(isset($post['form'])){// si le champ form exist
 				if($post['form'] == 'co'){//si le champ form vaux 'co'
-					// ici le trétement pour le formulaire de connexion
+					// ici le traitement pour le formulaire de connexion
 					if(isset($post['co_pseudo'])){
 						if(preg_match('#^[A-Z]{1}[A-Za-z0-9.-_]{2,15}$#', $post['co_pseudo']) == 0){
 							$errors[] = 'Erreur pseudo co';
 						}
 					}
 					if(isset($post['co_pswd'])){
-						var_dump(preg_match('#([A-Z]{1}||[a-z]{1}||[0-9]{1}){8,20}#', $post['co_pswd']));
+						var_dump(preg_match('#([A-Z]{1}||[a-z]{1}||[0-9]{1}){8,20}#', $post['co_pswd']));//majuscule et chiffre
 						if(preg_match('#([A-Z]{1}&[a-z]{1}&[0-9]{1}){8,}#', $post['co_pswd']) == 0){
-							$errors[] = 'Erreur pswd co';
+							$errors[] = 'Votre mot de passe doit contenir au moins une majuscule et un chiffre.';
 						}
 					}
 
@@ -38,7 +38,7 @@ class DefaultController extends Controller
 					}
 				}
 				if($post['form'] == 'nl'){//si le champ form vaux 'nl'
-					// ici le trétement pour le formulaire de newsletter
+					// ici le traitement pour le formulaire de newsletter
 					if(isset($post['nl_email'])){
 						if(preg_match('#^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$#', $post['nl_email']) == 0){
 							$errors[] = 'Erreur email nl';
@@ -51,10 +51,10 @@ class DefaultController extends Controller
 					}
 				}
 				if($post['form'] == 'isc'){//si le champ form vaux 'isc'
-					// ici le trétement pour le formulaire d'inscription
+					// ici le traitement pour le formulaire d'inscription
 				}
 				if($post['form'] == 'ct'){//si le champ form vaux 'ct'
-					// ici le trétement pour le formulaire de contact
+					// ici le traitement pour le formulaire de contact
 				}
 			}
 		}
