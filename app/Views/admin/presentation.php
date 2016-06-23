b3f94d0cf176733050369906d4390f3dda404b34
BumbleBrice <collilieux.brice@gmail.com>
2016-06-23 17:12:03 +0200

    début section présentation

--- app/Views/admin/presentation.php
+++ app/Views/admin/presentation.php
@@ -2,5 +2,20 @@
 
 
 
-
+<div class="row">
+    <div class="box">
+        <div class="col-lg-12">
+            <hr>
+                <strong>Ôbar</strong>
+            </h2>
+            <hr>
+            <p class="text-center">Use as many boxes as you like, and put anything you want in them! They are great for just about anything, the sky's the limit!</p>
+        </div>
+    </div>
+</div>
+<div class="row ">
+	<div class="col-lg-offset-5 col-lg-2 col-lg-offset-5">
+           	<a type="button" class="btn btn-secondary btn-lg active" href="">Modifier la présentation de Ôbar</a>
+	</div>
+</div>
 
