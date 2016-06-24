3c6d848df7cca09d41ebcf346b847c3e8088fbe9
YoanGarcia <yoan.gcia@hotmail.fr>
2016-06-24 12:14:57 +0200

    MAJ routes

--- app/routes.php
+++ app/routes.php
@@ -7,24 +7,24 @@
 
 
 		// Partie admin
-		['GET|POST', '/admin_home', 'admin#home', 'home_admin'],
-		['GET|POST', '/admin_presentation_edit', 'admin#presentation_edit', 'admin_presentation_edit'],
-		['GET|POST', '/admin_slide_edit', 'admin#slide_edit', 'admin_slide_edit'],
-		['GET|POST', '/admin_slide_delete', 'admin#slide_delete', 'admin_slide_delete'],
+		['GET|POST', '/admin_home', 'adminHome#home', 'home_admin'],
+		['GET|POST', '/admin_presentation_edit', 'adminHome#presentation_edit', 'admin_presentation_edit'],
+		['GET|POST', '/admin_slide_edit', 'adminHome#slide_edit', 'admin_slide_edit'],
+		['GET|POST', '/admin_slide_delete', 'adminHome#slide_delete', 'admin_slide_delete'],
 
-		['GET|POST', '/admin_bar_list', 'admin#bar_list', 'admin_bar_list'],
-		['GET|POST', '/admin_bar_add', 'admin#bar_add', 'admin_bar_add'],
-		['GET|POST', '/admin_bar_edit', 'admin#bar_edit', 'admin_bar_edit'],
-		['GET|POST', '/admin_bar_delete', 'admin#bar_delete', 'admin_bar_delete'],
+		['GET|POST', '/admin_bar_list', 'adminBar#bar_list', 'admin_bar_list'],
+		['GET|POST', '/admin_bar_add', 'adminBar#bar_add', 'admin_bar_add'],
+		['GET|POST', '/admin_bar_edit', 'adminBar#bar_edit', 'admin_bar_edit'],
+		['GET|POST', '/admin_bar_delete', 'adminBar#bar_delete', 'admin_bar_delete'],
 
-		['GET|POST', '/admin_message', 'admin#message', 'admin_message'],
+		['GET|POST', '/admin_message', 'adminMessage#message', 'admin_message'],
 
-		['GET|POST', '/admin_users', 'admin#users', 'admin_users'],
-		['GET|POST', '/admin_user_add', 'admin#user_add', 'admin_user_add'],
-		['GET|POST', '/admin_user_edit', 'admin#user_edit', 'admin_user_edit'],
-		['GET|POST', '/admin_user_delete', 'admin#user_delete', 'admin_user_delete'],
+		['GET|POST', '/admin_users', 'adminUsers#users', 'admin_users'],
+		['GET|POST', '/admin_user_add', 'adminUsers#user_add', 'admin_user_add'],
+		['GET|POST', '/admin_user_edit', 'adminUsers#user_edit', 'admin_user_edit'],
+		['GET|POST', '/admin_user_delete', 'adminUsers#user_delete', 'admin_user_delete'],
 
-		['GET|POST', '/admin_newsletter', 'admin#newsletter', 'admin_newsletter'],
+		['GET|POST', '/admin_newsletter', 'adminNews#newsletter', 'admin_newsletter'],
 
 
 
