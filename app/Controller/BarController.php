bd6cb334c890ceb5e9b3a2b2c8e5f7f525d9ffcd
AlexisMeunier <meunier_33@live.fr>
2016-06-24 15:14:08 +0200

    aze

--- app/Controller/BarController.php
+++ app/Controller/BarController.php
@@ -32,7 +32,7 @@ class BarController extends Controller
 	public function bar_add()
 	{
 			// On limite l'accé à la page aux utilisateurs authentifiés et à ceux dont le rôle est admin
-			$this->allowTo(['admin']);
+			// $this->allowTo(['admin']);
 
 			$errors = [];
 			$success = false;
