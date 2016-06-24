d726bfe55353ab8647879059f8c5886696cbd7a3
AlexisMeunier <meunier_33@live.fr>
2016-06-24 09:46:58 +0200

    aze

--- app/Controller/MessageController.php
+++ app/Controller/MessageController.php
@@ -43,9 +43,32 @@ class MessageController extends Controller
 
 		$message->update($data, $id);
 	}
-	public function answerMessage()
+	public function answerMessage($email, $reponse)
 	{
+		$mail = new PHPMailer;
 
+		$mail->isSMTP();                                      // Set mailer to use SMTP
+		$mail->Host = 'smtp.mailgun.org';  // Specify main and backup SMTP servers
+		$mail->SMTPAuth = true;                               // Enable SMTP authentication
+		$mail->Username = 'user@example.com';                 // SMTP username
+		$mail->Password = 'secret';                           // SMTP password
+		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+		$mail->Port = 587;                                    // TCP port to connect to
+
+		$mail->setFrom('from@example.com', 'Mailer');
+		$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+
+		$mail->isHTML(true);                                  // Set email format to HTML
+
+		$mail->Subject = 'Here is the subject';
+		$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+		if($mail->send()) {
+		    return true;
+		} else {
+		    return false;
+		}
 	}
 
 }
