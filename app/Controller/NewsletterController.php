4eca832048c0d8ac6aefeaa7a4aa17f769852eca
AlexisMeunier <meunier_33@live.fr>
2016-06-26 13:01:59 +0200

    aze

--- app/Controller/NewsletterController.php
+++ app/Controller/NewsletterController.php
@@ -7,11 +7,12 @@ use \Model\NewsletterModel as Newsletter;
 
 class NewsletterController extends Controller
 {
-	$Newsletter = new Newsletter();
+	public function newsletter(){
+		$this->show('adminNews/news_letter');
+	}
 
 	public function add_subscribers($email){
-
-		
+		$Newsletter = new Newsletter();
 
 		$data = ['email' => $email];
 
