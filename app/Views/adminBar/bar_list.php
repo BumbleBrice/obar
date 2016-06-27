226a973d98bad570e52fa39bf7e6b1c7b8aceffd
YoanGarcia <yoan.gcia@hotmail.fr>
2016-06-27 11:38:57 +0200

    eza

--- app/Views/adminBar/bar_list.php
+++ app/Views/adminBar/bar_list.php
@@ -11,27 +11,39 @@
 <br>
 <div>
 	
-	<?php foreach($bars as $bar): ?>
-		<div class="container">
-
+		<div class="container divGlobalListBar">
 			<div class="row">
-				<div class="col-lg-12 text-center">
-					<h3 class="nameListBar"><?=$bar['name']; ?></h3>
-				</div>
+
+				<?php foreach($bars as $bar): ?>
+
+					<div class="col-lg-5 divListBar text-center">
+						<div class="row">
+							<div class="col-lg-12 text-center">
+								<h3 class="nameListBar"><?=$bar['name']; ?></h3>
+							</div>
+						</div>
+
+						<br><br>
+						<div class="row">
+							<div class="col-lg-6">
+								<img class="imgListBar" src="<?=$bar['picture']; ?>">
+							</div>
+
+							<div class="col-lg-6">
+								<p class="contentListBar"><?=$bar['description']; ?></p>
+							</div>				
+						</div>
+
+						<br>
+						<div class="col-lg-12 text-center">
+							<a class="btnListBar" href="<?= $this->url('admin_bar_edit', ['id' => $bar['id']]) ?>">Modifier le bar</a>
+							<a class="btnListBar" href="<?= $this->url('admin_bar_delete', ['id' => $bar['id']]) ?>">Supprimer le bar</a>
+						</div>
+					</div>
+
+				<?php endforeach; ?>
+
 			</div>
-
-			<br><br>
-			<div class="col-lg-6"></div>
-			<p class="contentListBar"><?=$bar['description']; ?></p>
-
-			<br><br>
-			<img src="<?=$bar['picture']; ?>" class="imgListBar" >
-
-			<br><br>
-			<a class="btnListBar" href="<?= $this->url('admin_bar_edit', ['id' => $bar['id']]) ?>">Modifier le bar</a>
-			<a class="btnListBar" href="<?= $this->url('admin_bar_delete', ['id' => $bar['id']]) ?>">Supprimer le bar</a>
-
 		</div>
-	<?php endforeach; ?>
 
 <?php $this->stop('main_content') ?>
