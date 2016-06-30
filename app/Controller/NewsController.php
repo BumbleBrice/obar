fe9ffe0f5daa47efe60612d22e8625f4c85cf646
BumbleBrice <collilieux.brice@gmail.com>
2016-06-30 17:44:36 +0200

    changement de l'ordre d'apparition des messages coté admin

--- app/Controller/NewsController.php
+++ app/Controller/NewsController.php
@@ -20,11 +20,69 @@ class NewsController extends Controller
 
 		$params = [];
 		
-		$params['news3'] = $newsModel->findAll('id', 'ASC', 3);
-		$params['news'] = $newsModel->findAll('id', 'ASC', 150, 4);
+		$params['news3'] = $newsModel->findAll('id', 'DESC', 3);
+		$params['news'] = $newsModel->findAll('id', 'DESC', 1500, 3);
 
 
 			
 		$this->show('adminNews/news', $params);
 	} /*Fin de la fonction news*/
+
+	public function news_add()
+	{
+			// On limite l'accé à la page aux utilisateurs authentifiés et à ceux dont le rôle est admin
+			// $this->allowTo(['admin']);
+
+			// On instancie la classe newsModel qui étend la classe Model
+			$newsModel = new newsModel();
+
+			$errors = [];
+			$success = false;
+
+			if(!empty($_POST)){
+				$post = array_map('trim', array_map('strip_tags', $_POST));
+
+				if(isset($post['what'])){
+					if(preg_match('#^.{1,}$#', $post['what']) == 0){
+						$errors[] = 'error name';
+					}
+				}
+
+				if(isset($post['bar'])){
+					if(preg_match('#^.{1,}$#', $post['bar']) == 0){
+						$errors[] = 'error quartiers';
+					}
+				}
+
+				if(isset($post['msg'])){
+					if(preg_match('#^.{1,}$#', $post['msg']) == 0){
+						$errors[] = 'error picture';
+					}
+				}
+
+				if (count($errors) == 0) {
+					// Ici il n'y a aucune erreurs, on peut donc enregistrer en base de donnée
+
+					// On utilise la méthode insert() qui permet d'insérer des données en base de donnée
+					$data = [
+						// La clé du tableau correspond au nom de la colonne SQL
+						'what' => $post['what'],
+						'bar'  => $post['bar'],
+						'msg'  => $post['msg'],
+					];
+
+					// On passe le tableau $data à la méthode insert() pur enregistrer nos données en bdd
+					if ($newsModel->insert($data)) {
+
+						// Ici l'insertion en base est effectuée
+						$success = true;
+					}
+				}
+			}
+
+			// On envoie les erreurs en paramètre à l'aide d'un tableau (array)
+			$params = ['errors' => $errors, 'success' => $success];
+
+		$this->show('adminNews/news_add', $params);
+	}
 }
